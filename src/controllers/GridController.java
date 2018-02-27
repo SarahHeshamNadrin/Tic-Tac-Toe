80581fd786ad805ef057c4ab653ec8bfb8bd68cd
Mohamed Gamal <m.gemyero91@gmail.com>
2018-02-27 06:19:34 +0200

    multiplayer on local

--- src/controllers/GridController.java
+++ src/controllers/GridController.java
@@ -5,8 +5,6 @@ package controllers;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
@@ -16,6 +14,9 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import Main.Game;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 /**
  * FXML Controller class
@@ -26,37 +27,56 @@ public class GridController implements Initializable {
 
     @FXML
     private GridPane board;
-    
+    private Game game;
+
     /**
-     * 
+     *
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-
+        game = new Game();
         System.out.println("initialize function");
 
-   
-    }    
+    }
 
     @FXML
     private void mouseClick(MouseEvent event) {
-        
-       System.out.println("mouse pressed");
-       Image x =new Image("/views/imgs/woodX.jpg");
-       
-        Node source = (Node)event.getTarget();
-        //System.out.println(source);
-       // System.out.println(event.getTarget());
-        //System.out.println(event.getTarget());
+
+        Node source = (Node) event.getTarget();
+
         Integer colIndex = GridPane.getColumnIndex(source);
         Integer rowIndex = GridPane.getRowIndex(source);
-        System.out.println("col Index:"+colIndex);
-        System.out.println("row Index:"+rowIndex);
-        ImageView m1 = (ImageView)source;
-        m1.setImage(x);
-        
+        try {
+            if (game.isMoveAvailable(rowIndex, colIndex)) {
+                if (game.getTurn() == Game.State.X) {
+                    setImage("/views/imgs/woodX.jpg", source);
+                } else if (game.getTurn() == Game.State.O) {
+                    setImage("/views/imgs/woodO.jpg", source);
+                }
+                game.move(rowIndex, colIndex);
+                if (game.isGameOver()) {
+                    Alert alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Information Dialog");
+                    alert.setHeaderText(null);
+                    if (game.getWinner() == Game.State.X || game.getWinner() == Game.State.O) {
+                        alert.setContentText("Player " + game.getWinner() + " wins");
+                    } else {
+                        alert.setContentText("Draw Game");
+                    }
+                    alert.showAndWait();
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid move!");
+        }
     }
-    
+
+    private void setImage(String image, Node source) {
+        Image img = new Image(image);
+        ImageView iv = (ImageView) source;
+        iv.setImage(img);
+    }
+
 }
